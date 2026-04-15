a4d47c56a16554d0b78400c48a13b78481d28c68
rodrigojpsilva <rodrigojrpsilva@gmail.com>
2026-04-15 07:44:34 -0300

    refactor(repository): improve repositories good practices

--- security/src/main/java/com/dm3/security/repository/PedidoRepository.java
+++ security/src/main/java/com/dm3/security/repository/PedidoRepository.java
@@ -1,6 +1,6 @@
 package com.dm3.security.repository;
 
-import com.dm3.security.entity.Pedido;
+import com.example.ecomerce.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
